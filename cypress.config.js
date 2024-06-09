42468a820e12c00e9157ed703e89abf1485c3f47
CheetahCodes21 <mcpai217@gmail.com>
2024-06-09 19:46:06 +0530

    Test herokuapp login logout

---
+++ cypress.config.js
@@ -0,0 +1,19 @@
+const { defineConfig } = require("cypress");
+
+async function setupNodeEvents(on, config) {
+  // implement node event listeners here
+
+
+  return config;
+}
+
+    
+  module.exports = defineConfig({
+    
+  e2e: {
+    setupNodeEvents,
+    specPattern:'cypress/testDemo/myFirstTest.cy.js',
+
+
+  },
+});
---
+++ cypress/testDemo/myFirstTest.cy.js
@@ -0,0 +1,9 @@
+describe("myFirstTestCase", () => {
+    it.only("myFirstTest", () => {
+      cy.visit("https://the-internet.herokuapp.com/login");
+      cy.get("#username").type("tomsmith");
+      cy.get("#password").type("SuperSecretPassword!");
+      cy.contains(" Login").click();
+      cy.contains(" Logout").click();
+    });
+  });
